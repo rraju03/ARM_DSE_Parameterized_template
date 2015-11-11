d7b2e0e310e65f93923d4c519df7a8b141244da9
Ramesh <rameshkumar.raju@safeway.com>
2015-11-10 19:01:44 -0800

    updated the command with new file names

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 RESOURCE_GROUP=$1
 
 azure group create $1 "West US"
-azure group deployment create -f mainTemplate.json -e mainTemplateParameters.json $RESOURCE_GROUP dse
+azure group deployment create -f DSE-mainTemplate.json -e DSE-mainTemplate.param.dev.json $RESOURCE_GROUP dse
 
